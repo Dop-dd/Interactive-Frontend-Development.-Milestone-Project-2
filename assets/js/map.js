b64e40abad6dd7c4b4e46dfdb52000a776ac12f4
Dop-dd <dopdd@yahoo.com>
2020-06-16 07:42:35 +0000

    change map code

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,216 +1,136 @@
-/*--------Create variables--------*/
-
-let my_location, map, bounds, infoWindow, currentInfoWindow, service, infoPane;
+/**----Google Maps locator------------ */
+var map;
 
 function initMap() {
-  // Initialize variables
-  bounds = new google.maps.LatLngBounds();
-  infoWindow = new google.maps.InfoWindow();
-  currentInfoWindow = infoWindow;
-
-  // Add a sidebar with panel as id
-  infoPane = document.getElementById("panel");
-
-  /* Try HTML Geolocation. 
-    Source code at: https://developers.google.com/maps/documentation/javascript/examples/map-geolocation
-    */
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        my_location = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        map = new google.maps.Map(document.getElementById("map"), {
-          center: my_location,
-          zoom: 15,
-        });
-        bounds.extend(my_location);
-
-        infoWindow.setPosition(my_location);
-        infoWindow.setContent("Location found.");
-        infoWindow.open(map);
-        map.setCenter(my_location);
-
-        // Call Places Nearby Search on user's location
-        getNearbyPlaces(my_location);
-      },
-      () => {
-        // Browser supports geolocation, but user has denied permission
-        handleLocationError(true, infoWindow);
-      }
-    );
-  } else {
-    // Browser doesn't support geolocation
-    handleLocationError(false, infoWindow);
-  }
-
-  /*---------- --------Search Box-------------------------------------- */
-  var input = document.getElementById("user-input");
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", function () {
-    searchBox.setBounds(map.getBounds());
+  // Create the map.
+  var pyrmont = { lat: 51.2194, lng: 4.4025 };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 17
   });
 
-  /*------------------------------------------------------------*/
-}
-
-// Handle a geolocation error
-function handleLocationError(browserHasGeolocation, infoWindow) {
-  // Set my default location to ntwerp, Belgium
-  my_location = { lat: 51.219, lng: 4.402 };
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: my_location,
-    zoom: 13,
-  });
-
-  // Display an InfoWindow at the map center : Example at: https://developers.google.com/maps/documentation/javascript/geolocation
-  infoWindow.setPosition(my_location);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Geolocation permissions denied. Using default location."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-  currentInfoWindow = infoWindow;
-
-  // Call Places Nearby Search on the default location
-  getNearbyPlaces(my_location);
-}
-
-/* Perform a Places Nearby Search Request: Link at 
-    https://developers.google.com/maps/documentation/javascript/examples/place-search: Requires the Places library
-    */
-
-function getNearbyPlaces(position) {
-  let request = {
-    location: position,
-    rankBy: google.maps.places.RankBy.DISTANCE,
-    keyword: "hotel",
+  // Create the places service.
+  var service = new google.maps.places.PlacesService(map);
+  var getNextPage = null;
+  var moreButton = document.getElementById('more');
+  moreButton.onclick = function () {
+    moreButton.disabled = true;
+    if (getNextPage) getNextPage();
   };
 
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, nearbyCallback);
-}
+  // Perform a nearby search.
+  var input = document.getElementById('searchInput');
+  
+  service.nearbySearch(
+    { location: pyrmont, radius: 500, type: ['metro'] },
+    function (results, status, pagination) {
+      if (status !== 'OK') return;
 
-// Handle the results of the Nearby Search
-function nearbyCallback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    createMarkers(results);
-  }
-}
-
-// Set markers at the location of each place result
-function createMarkers(places) {
-  places.forEach((place) => {
-    let marker = new google.maps.Marker({
-      position: place.geometry.location,
-      map: map,
-      title: place.name,
-    });
-
-    // Add click listener to each marker: Link at: https://developers.google.com/maps/documentation/javascript/events
-    google.maps.event.addListener(marker, "click", () => {
-      let request = {
-        placeId: place.place_id,
-        fields: [
-          "name",
-          "formatted_address",
-          "geometry",
-          "rating",
-          "website",
-          "photos",
-        ],
+      createMarkers(results);
+      moreButton.disabled = !pagination.hasNextPage;
+      getNextPage = pagination.hasNextPage && function () {
+        pagination.nextPage();
       };
+    });
+/*------ Auto complete--Example code at: 
+https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch */
 
-      /* Only fetch the details of a place when the user clicks on a marker.
-           Examples at: https://developers.google.com/maps/documentation/javascript/places */
-      service.getDetails(request, (placeResult, status) => {
-        showDetails(placeResult, marker, status);
-      });
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  /* Add Listener event. Code from https://developers.google.com/maps/documentation/javascript/events */
+
+  autocomplete.addListener('place_changed', function () {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+    marker.setIcon(({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+
+    // Location details  Example code from : https://developers.google.com/maps/documentation/javascript/places
+    for (var i = 0; i < place.address_components.length; i++) {
+      if (place.address_components[i].types[0] == 'postal_code') {
+        document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+      }
+      if (place.address_components[i].types[0] == 'country') {
+        document.getElementById('country').innerHTML = place.address_components[i].long_name;
+      }
+    }
+    document.getElementById('location').innerHTML = place.formatted_address;
+    document.getElementById('latt').innerHTML = place.geometry.location.lat();
+    document.getElementById('long').innerHTML = place.geometry.location.lng();
+  });
+
+}
+
+function createMarkers(places) {
+  var bounds = new google.maps.LatLngBounds();
+  var placesList = document.getElementById('places');
+
+  for (var i = 0, place; place = places[i]; i++) {
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
     });
 
-    // Adjust the map bounds to include the location of this marker
+    var li = document.createElement('li');
+    li.textContent = place.name;
+    placesList.appendChild(li);
+
     bounds.extend(place.geometry.location);
-  });
-  /*  adjust the bounds of the map to show all the markers within the visible area. */
+  }
   map.fitBounds(bounds);
 }
 
-// Builds an InfoWindow to display details above the marker
-function showDetails(placeResult, marker, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    let placeInfowindow = new google.maps.InfoWindow();
-    let rating = "None";
-    if (placeResult.rating) rating = placeResult.rating;
-    placeInfowindow.setContent(
-      "<div><strong>" +
-        placeResult.name +
-        "</strong><br>" +
-        "Rating: " +
-        rating +
-        "</div>"
-    );
-    placeInfowindow.open(marker.map, marker);
-    currentInfoWindow.close();
-    currentInfoWindow = placeInfowindow;
-    showPanel(placeResult);
-  } else {
-    console.log("showDetails failed: " + status);
-  }
-}
 
-// Displays place details in a sidebar
-function showPanel(placeResult) {
-  // If infoPane is already open, close it
-  if (infoPane.classList.contains("open")) {
-    infoPane.classList.remove("open");
-  }
-
-  // Clear the previous details
-  while (infoPane.lastChild) {
-    infoPane.removeChild(infoPane.lastChild);
-  }
-
-  // Add the primary photo, if found: Examples at https://developers.google.com/places/web-service/photos
-  if (placeResult.photos) {
-    let firstPhoto = placeResult.photos[0];
-    let photo = document.createElement("img");
-    photo.classList.add("hero");
-    photo.src = firstPhoto.getUrl();
-    infoPane.appendChild(photo);
-  }
-
-  // Add place details with text formatting
-  let name = document.createElement("h1");
-  name.classList.add("place");
-  name.textContent = placeResult.name;
-  infoPane.appendChild(name);
-  if (placeResult.rating) {
-    let rating = document.createElement("p");
-    rating.classList.add("details");
-    rating.textContent = `Rating: ${placeResult.rating} \u272e`;
-    infoPane.appendChild(rating);
-  }
-
-  let address = document.createElement("p");
-  address.classList.add("details");
-  address.textContent = placeResult.formatted_address;
-  infoPane.appendChild(address);
-  if (placeResult.website) {
-    let websitePara = document.createElement("p");
-    let websiteLink = document.createElement("a");
-    let websiteUrl = document.createTextNode(placeResult.website);
-    websiteLink.appendChild(websiteUrl);
-    websiteLink.title = placeResult.website;
-    websiteLink.href = placeResult.website;
-    websitePara.appendChild(websiteLink);
-    infoPane.appendChild(websitePara);
-  }
-
-  // Open the infoPane
-  infoPane.classList.add("open");
-}
